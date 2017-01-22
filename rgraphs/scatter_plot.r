2396c3f922c426e72d472df6244ef8410fe5ddbb
David Sterry <davids@exchb.com>
2017-01-21 20:32:32 -0800

    output as png

--- rgraphs/scatter_plot.r
+++ rgraphs/scatter_plot.r
@@ -1,4 +1,6 @@
 tx <- read.csv(file="transaction_info.csv",sep=",",head=TRUE)
+png(filename = "bitcoin-fees.png",
+    width = 2560, height = 1440, units = "px", pointsize = 12)
 plot(tx$fee_rate,tx$conf_time, log="xy", yaxt="n", xaxt="n", pch = 20, cex=0.05)
 marks <- c(0,60,600,3600,86400)
 axis(2,at=marks,labels=marks)
